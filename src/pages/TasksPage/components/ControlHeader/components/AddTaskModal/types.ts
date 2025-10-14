b358ec40f5c409f2ad6625f489e599d2655bcfee
s1rserg <sirserg05@gmail.com>
2025-10-14 13:26:08 +0300

    refactor(add-task-form): refactor create task form data zod infer

--- src/pages/TasksPage/components/ControlHeader/components/AddTaskModal/types.ts
+++ src/pages/TasksPage/components/ControlHeader/components/AddTaskModal/types.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import type { infer as ZodInfer } from 'zod';
 import type { CreateTaskSchema } from './schema';
 
-export type CreateTaskFormData = z.infer<typeof CreateTaskSchema>;
+export type CreateTaskFormData = ZodInfer<typeof CreateTaskSchema>;
