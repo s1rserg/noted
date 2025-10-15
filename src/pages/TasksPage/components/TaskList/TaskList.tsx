72fb49c048e36e072e1e64dab7bc68ea2e25a708
Serhii Serhiienko <sirserg05@gmail.com>
2025-10-15 16:18:42 +0300

    feat(tasks-actions): add tasks actions

--- src/pages/TasksPage/components/TaskList/TaskList.tsx
+++ src/pages/TasksPage/components/TaskList/TaskList.tsx
@@ -7,20 +7,17 @@ import type { ViewMode } from '../../types';
 interface TaskListProps {
   tasks: Task[];
   mode: ViewMode;
+  onCompleteTask: (id: string) => void;
+  onDeleteTask: (id: string) => void;
 }
 
-export const TaskList: FC<TaskListProps> = memo(({ tasks }) => {
-  const handleComplete = (id: string) => alert(`Complete task: ${id}`);
-  const handleDelete = (id: string) => {
-    alert(`Delete task: ${id}`);
-  };
-
+export const TaskList: FC<TaskListProps> = memo(({ tasks, onCompleteTask, onDeleteTask }) => {
   return (
     <Box>
       <Grid container spacing={2}>
         {tasks.map((task) => (
           <Grid key={task.id} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-            <TaskCard task={task} onComplete={handleComplete} onDelete={handleDelete} />
+            <TaskCard task={task} onComplete={onCompleteTask} onDelete={onDeleteTask} />
           </Grid>
         ))}
       </Grid>
