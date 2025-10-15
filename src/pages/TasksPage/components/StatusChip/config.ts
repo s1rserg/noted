c458b5b7bdd4242226ad9339f20d4d2d9ff38abb
s1rserg <sirserg05@gmail.com>
2025-10-15 12:04:32 +0300

    refactor(tasks-grid-view): optimize type import

--- src/pages/TasksPage/components/StatusChip/config.ts
+++ src/pages/TasksPage/components/StatusChip/config.ts
@@ -1,7 +1,8 @@
 import { CheckCircle, Circle, RadioButtonUnchecked } from '@mui/icons-material';
+import type { ElementType } from 'react';
 import { TaskStatus, type TaskStatusValues } from 'types/task';
 
-export const StatusIcons: Record<TaskStatusValues, React.ElementType> = {
+export const StatusIcons: Record<TaskStatusValues, ElementType> = {
   [TaskStatus.PENDING]: RadioButtonUnchecked,
   [TaskStatus.IN_PROGRESS]: Circle,
   [TaskStatus.COMPLETED]: CheckCircle,
