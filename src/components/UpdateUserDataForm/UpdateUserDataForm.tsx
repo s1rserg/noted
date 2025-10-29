79dc791b35e339898a1a5f28c9d38d0f06cae7cb
s1rserg <sirserg05@gmail.com>
2025-10-29 17:51:00 +0200

    feat(update-user-data-form): add update user data form

--- src/components/UpdateUserDataForm/UpdateUserDataForm.tsx
+++ src/components/UpdateUserDataForm/UpdateUserDataForm.tsx
@@ -2,40 +2,49 @@ import { Box, Button, CircularProgress, Stack, Typography } from '@mui/material'
 import { useForm, type Resolver } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { UpdateUserSchema, type UpdateUserDto } from 'api';
-import { type FC } from 'react';
-import { Step2DefaultValues } from './config';
+import { type FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FormInput } from 'components/FormInput';
+import { UpdateUserDataDefaultValues } from './config';
 
 interface Props {
   onSubmit: (updateData: UpdateUserDto) => Promise<boolean>;
-  onSkip: () => void;
+  onCancel: () => void;
   isLoading: boolean;
+  initialData?: Partial<UpdateUserDto>;
+  title?: string;
+  submitButtonText?: string;
+  cancelButtonText?: string;
 }
 
-export const Step2Form: FC<Props> = ({ onSubmit, onSkip, isLoading }) => {
-  const { t } = useTranslation('registerPage');
+export const UpdateUserDataForm: FC<Props> = ({
+  onSubmit,
+  onCancel,
+  isLoading,
+  initialData,
+  title,
+  submitButtonText,
+  cancelButtonText,
+}) => {
+  const { t } = useTranslation();
 
   const {
     control,
     handleSubmit,
     reset,
     clearErrors,
-    formState: { errors },
+    formState: { errors, isDirty },
   } = useForm<UpdateUserDto>({
     resolver: zodResolver(UpdateUserSchema) as Resolver<UpdateUserDto>,
-    defaultValues: Step2DefaultValues,
+    defaultValues: initialData || UpdateUserDataDefaultValues,
     reValidateMode: 'onSubmit',
   });
 
+  useEffect(() => {
+    reset(initialData);
+  }, [initialData, reset]);
+
   const handleFormSubmit = handleSubmit(async (data: UpdateUserDto) => {
-    const isDataProvided = Object.values(data).some((value) => value !== '' && value !== undefined);
-
-    if (!isDataProvided) {
-      onSkip();
-      return;
-    }
-
     if (await onSubmit(data)) {
       reset();
     }
@@ -45,14 +54,14 @@ export const Step2Form: FC<Props> = ({ onSubmit, onSkip, isLoading }) => {
     <Box component="form" onSubmit={(e) => void handleFormSubmit(e)} noValidate>
       <Stack spacing={2}>
         <Typography variant="h5" component="h1" gutterBottom>
-          {t('step2.title')}
+          {title}
         </Typography>
 
         <FormInput
           control={control}
           clearErrors={clearErrors}
           name="name"
-          label={t('step2.labels.name')}
+          label={t('updateUserData.labels.name')}
           fullWidth
           autoComplete="given-name"
           margin="normal"
@@ -63,7 +72,7 @@ export const Step2Form: FC<Props> = ({ onSubmit, onSkip, isLoading }) => {
           control={control}
           clearErrors={clearErrors}
           name="surname"
-          label={t('step2.labels.surname')}
+          label={t('updateUserData.labels.surname')}
           fullWidth
           autoComplete="family-name"
           margin="normal"
@@ -74,7 +83,7 @@ export const Step2Form: FC<Props> = ({ onSubmit, onSkip, isLoading }) => {
           control={control}
           clearErrors={clearErrors}
           name="birthday"
-          label={t('step2.labels.birthday')}
+          label={t('updateUserData.labels.birthday')}
           type="date"
           fullWidth
           margin="normal"
@@ -89,20 +98,20 @@ export const Step2Form: FC<Props> = ({ onSubmit, onSkip, isLoading }) => {
             type="button"
             fullWidth
             variant="outlined"
-            onClick={onSkip}
+            onClick={onCancel}
             disabled={isLoading}
             sx={{ mt: 2, py: 1.5 }}
           >
-            {t('step2.buttons.skip')}
+            {cancelButtonText}
           </Button>
           <Button
             type="submit"
             fullWidth
             variant="contained"
-            disabled={isLoading}
+            disabled={isLoading || !isDirty}
             sx={{ mt: 2, py: 1.5 }}
           >
-            {isLoading ? <CircularProgress size={24} color="inherit" /> : t('step2.buttons.save')}
+            {isLoading ? <CircularProgress size={24} color="inherit" /> : submitButtonText}
           </Button>
         </Stack>
       </Stack>
--- src/pages/RegisterPage/components/index.ts
+++ src/pages/RegisterPage/components/index.ts
@@ -1,2 +1,1 @@
 export { Step1Form } from './Step1Form/Step1Form';
-export { Step2Form } from './Step2Form/Step2Form';
